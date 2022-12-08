db0833a846be72cc7294fce4a3316b27f7eb42f1
Andreas Jonsson <ajdev8@gmail.com>
2022-12-08 13:19:21 +0100

    Install language servers

---
+++ node-install.sh
@@ -0,0 +1,10 @@
+# Install language servers
+npm install -g ansible-language-server
+npm install -g bash-language-server
+npm install -g pyright
+npm install -g yaml-language-server
+npm install -g vscode-langservers-extracted   # css, html, js, json, markdown
+
+# Install code linters
+#   js, html, ccs: ?
+# npm install -g eslint   # included in vscode-langservers-extracted
