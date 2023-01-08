eaa8ce6d71db2df73be9ef14b810d1511e76c14d
Andreas Jonsson <ajdev8@gmail.com>
2023-01-08 22:26:21 +0100

    Disable some shellcheck warnings

--- node-install.sh
+++ node-install.sh
@@ -19,10 +19,12 @@ NVM_PROFILE="$HOME/.bashrc_local"
 export NVM_DIR="$HOME/.nvm" && (
   git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
+  # shellcheck disable=SC2006,SC2046
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
 
 SOURCE_STR="\\nexport NVM_DIR=\"${PROFILE_INSTALL_DIR}\"\\n[ -s \"\$NVM_DIR/nvm.sh\" ] && \\. \"\$NVM_DIR/nvm.sh\"  # This loads nvm\\n"
+# shellcheck disable=SC2016
 COMPLETION_STR='[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion\n'
 
 if ! command grep -qc '/nvm.sh' "$NVM_PROFILE"; then
